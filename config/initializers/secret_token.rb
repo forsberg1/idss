45b1c1fc96a495e4db7bb5e2f570f9986261d323
Joakim Forsberg <joakim.forsberg1@gmail.com>
2013-03-22 14:12:39 +0100

    init in git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdssSe::Application.config.secret_token = 'd3722a4bcf4107dd4079df83f020414282ce7a4be14cbf917dbd9ede4777efe4d7cad6205d946e75455359564f2efdaef9547f06adb7a83d6d9294a60f194679'
